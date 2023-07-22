3bd23877202a949aa71f7576fb7005813f082d4a
Кайдаров Павел <pavel.kaydarov@fabit.ru>
2023-07-22 14:58:01 +0300

    адаптив для хедера

--- src/pages-flat/HomePage/Sections/Hero/HeroImages.tsx
+++ src/pages-flat/HomePage/Sections/Hero/HeroImages.tsx
@@ -5,13 +5,13 @@ export const HeroImages = () => {
 	return (
 		<div className={cls.HeroImages}>
 			<div className={cls.grid1}>
-				<Image className={cls.HeroImage} src='/hero1.png' width={288} height={439} alt=''/>
+				<Image className={cls.HeroImage} src='/hero1.png' width={288} height={439} alt='' priority/>
 			</div>
 			<div className={cls.grid2}>
-				<Image className={cls.HeroImage} src='/hero2.png' width={413} height={216} alt=''/>
+				<Image className={cls.HeroImage} src='/hero2.png' width={413} height={216} alt='' priority/>
 			</div>
 			<div className={cls.grid3}>
-				<Image className={cls.HeroImage} src='/hero3.png' width={336} height={199} alt=''/>
+				<Image className={cls.HeroImage} src='/hero3.png' width={336} height={199} alt='' priority/>
 			</div>
 		</div>
 	);
--- src/widgets/Navigation/ui/Navigation.tsx
+++ src/widgets/Navigation/ui/Navigation.tsx
@@ -1,26 +1,41 @@
+'use client'
 import cls from './Navigation.module.scss';
 import classNames from "classnames";
 import Link from "next/link";
+import { useState } from "react";
 
 interface INavigationProps {
-    className?: string;
+	className?: string;
 }
 
 export const Navigation = (props: INavigationProps) => {
-    const { className } = props;
-    return (
-        <nav
-            className={classNames(cls.Navigation, {}, [className])}
-        >
-	        <Link className={cls.link} href='/'>
-		        Главная
-	        </Link>
-	        <Link className={cls.link} href='/services'>
-		        Услуги
-	        </Link>
-	        <Link className={cls.link} href='/contacts'>
-		        Контакты
-	        </Link>
-        </nav>
-    );
+	const {className} = props;
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	console.log(isOpen)
+	return (
+		<>
+			<label className={classNames(cls.hamburger, {[cls.open]: isOpen})}>
+				<input type={cls.checkbox} checked={isOpen}/>
+				<svg viewBox="0 0 32 32" onClick={() => setIsOpen(state => !state)}>
+					<path className={classNames(cls.line, {}, [cls.lineTopBottom])}
+					      d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
+					<path className={cls.line} d="M7 16 27 16"></path>
+				</svg>
+			</label>
+			<nav
+				className={classNames(cls.Navigation, {[cls.openNav]: isOpen}, [className])}
+			>
+
+				<Link className={cls.link} href='/'>
+					Главная
+				</Link>
+				<Link className={cls.link} href='/services'>
+					Услуги
+				</Link>
+				<Link className={cls.link} href='/contacts'>
+					Контакты
+				</Link>
+			</nav>
+		</>
+	);
 };
