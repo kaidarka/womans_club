6f160993b6917642a5993db104b8d68645c538c7
Кайдаров Павел <pavel.kaydarov@fabit.ru>
2023-07-23 01:12:07 +0300

    fix checked

--- src/widgets/Navigation/ui/Navigation.tsx
+++ src/widgets/Navigation/ui/Navigation.tsx
@@ -19,7 +19,7 @@ export const Navigation = (props: INavigationProps) => {
 	return (
 		<>
 			<label className={classNames(cls.hamburger, {[cls.open]: isOpen})}>
-				<input type={cls.checkbox} checked={isOpen}/>
+				<input type={cls.checkbox}/>
 				<svg viewBox="0 0 32 32" onClick={() => setIsOpen(state => !state)}>
 					<path className={classNames(cls.line, {}, [cls.lineTopBottom])}
 					      d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
